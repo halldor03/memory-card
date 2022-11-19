67a9763707da0ce94e703841fd3baaa911b9c38c
halldor03 <kacoleker@gmail.com>
2022-11-19 13:14:23 +0100

    Styling and sounds done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,113 @@
 import React, { useState } from "react";
 import Card from "./components/Card";
+import uniqid from "uniqid";
 
 export default function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState([
+    {
+      name: "Chicken",
+      imgSrc: "/img/ChickenImg.webp",
+      soundSrc: "/sounds/ChickenSound.oga",
+      id: uniqid(),
+    },
+    {
+      name: "Pig",
+      imgSrc: "/img/PigImg.webp",
+      soundSrc: "/sounds/PigSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Cow",
+      imgSrc: "/img/CowImg.webp",
+      soundSrc: "/sounds/CowSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Skeleton",
+      imgSrc: "/img/SkeletonImg.webp",
+      soundSrc: "/sounds/SkeletonSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Creeper",
+      imgSrc: "/img/CreeperImg.webp",
+      soundSrc: "/sounds/CreeperSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Ghast",
+      imgSrc: "/img/GhastImg.webp",
+      soundSrc: "/sounds/GhastSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Zombie",
+      imgSrc: "/img/ZombieImg.webp",
+      soundSrc: "/sounds/ZombieSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Villager",
+      imgSrc: "/img/VillagerImg.webp",
+      soundSrc: "/sounds/VillagerSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Spider",
+      imgSrc: "/img/SpiderImg.webp",
+      soundSrc: "/sounds/SpiderSound.oga",
+      id: uniqid(),
+    },
+    {
+      name: "Sheep",
+      imgSrc: "/img/SheepImg.webp",
+      soundSrc: "/sounds/SheepSound.oga",
+      id: uniqid(),
+    },
+    {
+      name: "Enderman",
+      imgSrc: "/img/EndermanImg.webp",
+      soundSrc: "/sounds/EndermanSound.ogg",
+      id: uniqid(),
+    },
+    {
+      name: "Witch",
+      imgSrc: "/img/WitchImg.webp",
+      soundSrc: "/sounds/WitchSound.ogg",
+      id: uniqid(),
+    },
+  ]);
+  const [soundtrack, setSoundtrack] = useState("Play soundtrack");
+  const playSoundtrack = (e) => {
+    setSoundtrack("Stop Soundtrack");
+    console.log("hi");
+    const sound = new Audio("/sounds/MinecraftSoundtrack.mp3");
+    sound.volume = 0.8;
+    sound.play();
+  };
+  // console.log(cards);
   return (
     <>
       <header>
         <div className="mainTitle">Minecraft memory game</div>
         <div className="scoreBoard">
-          <div className="currentScore">Current score: 0</div>
-          <div className="bestScore">Best score: 0</div>
+          <div className="currentScore">Current score: {currentScore}</div>
+          <div className="bestScore">Best score: {bestScore}</div>
         </div>
       </header>
-      <div className="gameInfo">
-        Get points by clicking on an image but don't click on any more than
-        once! Score 12 to win!
+      <div className="gameInfoContainer">
+        <div className="gameInfo">
+          Get points by clicking on an image but don't click on any more than
+          once! Score all 12 to win!
+        </div>
+        <button className="soundtrackButton" onClick={(e) => playSoundtrack()}>
+          {soundtrack}
+        </button>
       </div>
       <div className="cardContainer">
-        <Card />
-        {/* <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card /> */}
+        <Card cards={cards} />
       </div>
     </>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,79 +1,27 @@
 import React from "react";
-import Pig from "../img/Pig.webp";
-import Sheep from "../img/Sheep.webp";
-import Chicken from "../img/Chicken.webp";
-import Cow from "../img/Cow.webp";
-import Creeper from "../img/Creeper.webp";
-import Enderman from "../img/Enderman.webp";
-import Ghast from "../img/Ghast.webp";
-import Skeleton from "../img/Skeleton.webp";
-import Spider from "../img/Spider.webp";
-import Villager from "../img/Villager.webp";
-import Witch from "../img/Witch.webp";
-import Zombie from "../img/Zombie.webp";
 
 export default function Card(props) {
+  const { cards } = props;
+
+  const playSoundEffect = (mob) => {
+    const sound = new Audio(mob.soundSrc);
+    sound.play();
+  };
+
   return (
     <>
-      <div className="cardElement">
-        <img className="cardPhoto" src={Pig} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Sheep} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Chicken} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Cow} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Creeper} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Enderman} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Ghast} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Skeleton} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Spider} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Villager} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Witch} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
-
-      <div className="cardElement">
-        <img className="cardPhoto" src={Zombie} alt="Pig" />
-        <div className="cardTitle">Pig</div>
-      </div>
+      {cards.map((mob) => {
+        return (
+          <div
+            className="cardElement"
+            key={mob.id}
+            onClick={() => playSoundEffect(mob)}
+          >
+            <img className="cardPhoto" src={mob.imgSrc} alt={mob.name} />
+            <div className="cardTitle">{mob.name}</div>
+          </div>
+        );
+      })}
     </>
   );
 }
